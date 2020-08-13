84254e7609a71d38022957e8a921611dec042a59
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-13 07:06:28 +0300

    Improve generate-sample-config-sections script

--- telegraf/generate-sample-config-sections.sh
+++ telegraf/generate-sample-config-sections.sh
@@ -2,9 +2,14 @@
 
 readonly OUTPUT_DIR=generated/sample-sections
 readonly SECTIONS=(agent aggregators global_tags inputs outputs processors)
+readonly PATTERN='# for numbers and booleans they should be plain (ie, ${INT_VAR}, ${BOOL_VAR})'
 
 mkdir --parents $OUTPUT_DIR
 telegraf --version > $OUTPUT_DIR/telegraf.version
 for x in ${SECTIONS[@]}; do
-  telegraf --sample-config --section-filter $x > $OUTPUT_DIR/$x.conf
+  telegraf --sample-config --section-filter $x \
+    | sed "0,/$PATTERN/d" \
+    | tail -n +3 \
+    | head -n -1 \
+    > $OUTPUT_DIR/$x.conf
 done
