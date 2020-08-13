61162ecd0fd0e049270322791552cc4d8eaa39fc
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-13 06:57:50 +0300

    Add generate-config-outputs script

---
+++ telegraf/generate-config-outputs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+readonly OUTPUT_DIR=generated/outputs
+readonly PATTERN1="#                            OUTPUT PLUGINS                                   #"
+readonly PATTERN2="#                            PROCESSOR PLUGINS                                #"
+
+mkdir --parents $OUTPUT_DIR
+telegraf --version > $OUTPUT_DIR/telegraf.version
+for x in $(telegraf --output-list | tail -n +2); do
+  telegraf --output-filter $x config \
+    | sed "0,/$PATTERN1/d" \
+    | sed "/$PATTERN2/Q" \
+    | tail -n +4 \
+    | head -n -3 \
+    > $OUTPUT_DIR/outputs.$x.conf
+done
