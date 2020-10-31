43060d5cec3d4f29fb9b7eff7580143bf2becab2
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-10-31 16:25:05 +0300

    Add Iosevka font installer

---
+++ install/fonts/iosevka.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+readonly VERSION=3.7.1
+readonly TARGET_DIR=/usr/share/fonts/truetype/iosevka
+
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+  wget --quiet https://github.com/be5invis/Iosevka/releases/download/v$VERSION/ttf-iosevka-$VERSION.zip
+  unzip -qq ttf-iosevka-$VERSION.zip
+  mkdir --parents $TARGET_DIR
+  mv --force ttf/* $TARGET_DIR
+)
+rm --recursive --force $TEMP_DIR
+
+fc-cache
