93c0336e1c4bd5aa69b0f588a958063f7fc09346
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-01 00:27:27 +0300

    Add IntelliJ IDEA installer

---
+++ install/idea.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly VERSION=2020.2.3
+readonly STUFF=ideaIU-$VERSION.tar.gz
+
+readonly TARGET_DIR=$HOME/dev/idea
+mkdir --parents $TARGET_DIR
+
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+
+  echo -n Downloading...
+  wget --quiet https://download.jetbrains.com/idea/$STUFF
+  echo done
+
+  echo -n Extracting...
+  tar --extract --gzip --file=$STUFF
+  echo done
+
+  echo -n Installing...
+  mv --force idea-IU-202.7660.26/* $TARGET_DIR
+  echo done
+)
+rm --recursive --force $TEMP_DIR
