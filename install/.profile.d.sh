96f049854a1d0315dd7897939000fb088ef4776c
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-01 01:48:57 +0300

    Add GraalVM installer

---
+++ install/.profile.d.sh
@@ -0,0 +1,8 @@
+if [ -d $HOME/.profile.d ]; then
+  for i in $HOME/.profile.d/*.sh; do
+    if [ -r $i ]; then
+      . $i
+    fi
+  done
+  unset i
+fi
---
+++ install/graalvm.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly VERSION=20.2.0
+readonly BASE=graalvm-ce-java11
+readonly STUFF=$BASE-linux-amd64-$VERSION.tar.gz
+
+readonly TARGET_DIR=$HOME/dev/jdk
+mkdir --parents $TARGET_DIR
+
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+
+  echo -n Downloading...
+  wget --quiet https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-$VERSION/$STUFF
+  echo done
+
+  echo -n Extracting...
+  tar --extract --gzip --file=$STUFF
+  echo done
+
+  echo -n Installing...
+  mv --force $BASE-$VERSION $TARGET_DIR
+  echo done
+)
+rm --recursive --force $TEMP_DIR
+
+echo -n Configuring...
+ln --symbolic $TARGET_DIR/$BASE-$VERSION $TARGET_DIR/default
+sudo cp --force .profile.d.sh /etc/profile.d/profile.d.sh
+mkdir $HOME/.profile.d
+echo 'export JAVA_HOME=$HOME/dev/jdk/default
+export PATH=$JAVA_HOME/bin:$PATH
+' > $HOME/.profile.d/jdk.sh
+echo done
