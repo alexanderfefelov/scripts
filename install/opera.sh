7d690391f45404dbe489b0ebbeb5a8820da99faf
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-01 12:44:20 +0300

    Add Opera installer

---
+++ install/opera.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+
+  wget --quiet https://deb.opera.com/archive.key
+  apt-key add archive.key
+  add-apt-repository "deb https://deb.opera.com/opera-stable/ stable non-free"
+  apt-get -qq update
+  apt-get -qq install opera-stable
+)
+rm --recursive --force $TEMP_DIR
