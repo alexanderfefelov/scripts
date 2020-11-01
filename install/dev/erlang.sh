c5b86befe1ec75a0dbf99ce54acd5835478035eb
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-01 12:05:23 +0300

    Add Erlang installer

---
+++ install/dev/erlang.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+
+  wget --quiet http://binaries.erlang-solutions.com/ubuntu/erlang_solutions.asc
+  apt-key add erlang_solutions.asc
+  add-apt-repository "deb [arch=amd64] https://packages.erlang-solutions.com/ubuntu focal contrib"
+  apt-get -qq update
+  apt-get -qq install esl-erlang
+)
+rm --recursive --force $TEMP_DIR
