34c781f9dcfbbe47e507fb6d6ce1d081b90b42fd
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-02 16:10:04 +0300

    Add Rust installer

---
+++ install/dev/rust.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+
+  echo -n Downloading...
+  wget --quiet --output-document rustup-init.sh https://sh.rustup.rs
+  echo done
+
+  echo -n Installing...
+  bash rustup-init.sh -y --no-modify-path
+  echo done
+)
+rm --recursive --force $TEMP_DIR
+
+echo -n Configuring...
+sudo cp --force .profile.d.sh /etc/profile.d/profile.d.sh
+mkdir --parents $HOME/.profile.d
+echo "export PATH=\$HOME/.cargo/bin:\$PATH" > $HOME/.profile.d/rust.sh
+echo done
