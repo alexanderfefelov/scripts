b75623a02023faad46ca749203be79c8021b16f6
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-02 05:44:14 +0300

    Add Zeal installer

---
+++ install/dev/zeal.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+apt-get -qq update
+apt-get -qq install zeal
