e10fa3dc46f450f117a3aad0b13ec0ee23011232
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-03 14:04:18 +0300

    Implement Sumatra PDF desktop integration

--- install/install-sumatra-pdf.sh
+++ install/install-sumatra-pdf.sh
@@ -4,13 +4,55 @@
 # a list, or a compound command returns a non-zero status
 set -e
 
+readonly MONIKER=sumatra-pdf
 readonly VERSION=3.1.2
 readonly STUFF=SumatraPDF-$VERSION.zip
+readonly INSTALLER_DIR=$(dirname "$(realpath "$0")")
+readonly TARGET_DIR=$HOME/programs/$MONIKER
+readonly START_SCRIPT=$TARGET_DIR/start-$MONIKER.sh
 
-readonly TARGET_DIR=$HOME/programs/sumatra-pdf
+create_config_file() {
+  echo '# https://www.sumatrapdfreader.org/settings/settings3.2.html
+
+UiLanguage = en
+EbookUI [
+  # If true, the UI used for PDF documents will be used for ebooks as well (enables printing
+  # and searching, disables automatic reflow)
+  UseFixedPageUI = true
+]' > $TARGET_DIR/SumatraPDF-settings.txt
+}
+
+create_start_script() {
+  echo wine $TARGET_DIR/SumatraPDF.exe '"$@"' > $START_SCRIPT
+  chmod +x $START_SCRIPT
+}
+
+create_desktop_entry() { # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+  echo "[Desktop Entry]
+Type=Application
+Categories=Office;
+Name=Sumatra PDF
+Comment=
+Icon=$TARGET_DIR/$MONIKER.ico
+Exec=$START_SCRIPT %u
+Terminal=false" > $HOME/.local/share/applications/$MONIKER.desktop
+}
+
+register_mime_types() {
+  local -r MIME_TYPES="
+    application/epub+zip
+    application/pdf
+    application/x-fictionbook+xml
+    application/x-mobipocket-ebook
+    image/vnd.djvu
+    image/vnd.djvu+multipage
+  "
+  for x in $MIME_TYPES; do
+    xdg-mime default $MONIKER.desktop $x
+  done
+}
+
 mkdir --parents $TARGET_DIR
-readonly START_SCRIPT=$TARGET_DIR/start-sumatra-pdf.sh
-
 readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
 (
   cd $TEMP_DIR
@@ -25,11 +67,11 @@ readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
 
   echo -n Installing...
   mv --force SumatraPDF.exe $TARGET_DIR
-  chmod +x $TARGET_DIR/SumatraPDF.exe
-  echo 'wine "$(dirname "$(realpath "$0")")"/SumatraPDF.exe "$@"' > $START_SCRIPT
-  chmod +x $START_SCRIPT
+  cp --force $INSTALLER_DIR/$MONIKER.ico $TARGET_DIR
+  create_config_file
+  create_start_script
+  create_desktop_entry
+  register_mime_types
   echo done
 )
 rm --recursive --force $TEMP_DIR
-
-cp --force sumatra-pdf.ico $TARGET_DIR
