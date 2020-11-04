754fa3db7b035286db54ea4ef2493c2df4833017
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-04 09:39:06 +0300

    Implement Mattermost desktop integration

--- install/install-mattermost-desktop.sh
+++ install/install-mattermost-desktop.sh
@@ -9,6 +9,17 @@ readonly VERSION=4.6.1
 readonly STUFF=mattermost-desktop-$VERSION-linux-x64.tar.gz
 readonly TARGET_DIR=$HOME/programs/$MONIKER
 
+create_desktop_entry() { # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+  echo "[Desktop Entry]
+Type=Application
+Categories=Network;
+Name=Mattermost Desktop
+Comment=
+Icon=$TARGET_DIR/icon.svg
+Exec=$TARGET_DIR/mattermost-desktop
+Terminal=false" > $HOME/.local/share/applications/$MONIKER.desktop
+}
+
 if [ -d "$TARGET_DIR" ]; then
   echo Directory exists: $TARGET_DIR >&2
   exit 1
@@ -29,6 +40,7 @@ readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
 
   echo -n Installing...
   mv --force mattermost-desktop-$VERSION-linux-x64/* $TARGET_DIR
+  create_desktop_entry
   echo done
 )
 rm --recursive --force $TEMP_DIR
