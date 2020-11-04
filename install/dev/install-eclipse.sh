e64ae3d247b6f9e5197014444c4b1683ae47202b
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-04 21:10:14 +0300

    Add Eclipse installer

---
+++ install/dev/install-eclipse.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly MONIKER=eclipse
+readonly VERSION=2020-09
+readonly STUFF=eclipse-java-$VERSION-R-linux-gtk-x86_64.tar.gz
+readonly TARGET_DIR=$HOME/dev/$MONIKER
+
+create_desktop_entry() { # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+  echo "[Desktop Entry]
+Type=Application
+Categories=Development;
+Name=Eclipse
+Comment=
+Icon=$TARGET_DIR/icon.xpm
+Exec=$TARGET_DIR/eclipse
+Terminal=false" > $HOME/.local/share/applications/$MONIKER.desktop
+}
+
+if [ -d "$TARGET_DIR" ]; then
+  echo Directory exists: $TARGET_DIR >&2
+  exit 1
+fi
+
+mkdir --parents $TARGET_DIR
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+
+  echo -n Downloading...
+  wget --quiet "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$VERSION/R/$STUFF&mirror_id=1" --output-document=$STUFF
+  echo done
+
+  echo -n Extracting...
+  tar --extract --gzip --file=$STUFF
+  echo done
+
+  echo -n Installing...
+  mv --force eclipse/* $TARGET_DIR
+  create_desktop_entry
+  echo done
+)
+rm --recursive --force $TEMP_DIR
