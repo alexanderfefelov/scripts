7017b7c53e7830de1d2e99e68bd4a57e7933f152
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-04 09:11:13 +0300

    Implement DBeaver desktop integration

--- install/dev/install-dbeaver.sh
+++ install/dev/install-dbeaver.sh
@@ -9,6 +9,17 @@ readonly VERSION=7.2.3
 readonly STUFF=dbeaver-ce-$VERSION-linux.gtk.x86_64.tar.gz
 readonly TARGET_DIR=$HOME/dev/$MONIKER
 
+create_desktop_entry() { # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+  echo "[Desktop Entry]
+Type=Application
+Categories=Development;
+Name=DBeaver
+Comment=
+Icon=$TARGET_DIR/dbeaver.png
+Exec=$TARGET_DIR/dbeaver
+Terminal=false" > $HOME/.local/share/applications/$MONIKER.desktop
+}
+
 if [ -d "$TARGET_DIR" ]; then
   echo Directory exists: $TARGET_DIR >&2
   exit 1
@@ -29,6 +40,7 @@ readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
 
   echo -n Installing...
   mv --force dbeaver/* $TARGET_DIR
+  create_desktop_entry
   echo done
 )
 rm --recursive --force $TEMP_DIR
