8b8f767c1c8e6a47ceedd4e691b2b29b6c02ce38
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-04 07:02:03 +0300

    Implement Camunda Modeler desktop integration

--- install/install-camunda-modeler.sh
+++ install/install-camunda-modeler.sh
@@ -4,12 +4,43 @@
 # a list, or a compound command returns a non-zero status
 set -e
 
+readonly MONIKER=camunda-modeler
 readonly VERSION=4.3.0
 readonly STUFF=camunda-modeler-$VERSION-linux-x64.tar.gz
+readonly TARGET_DIR=$HOME/programs/$MONIKER
+readonly MIME_TYPES="
+  application/bpmn
+  application/cmmn
+  application/dmn
+"
 
-readonly TARGET_DIR=$HOME/programs/camunda-modeler
+create_desktop_entry() { # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+  echo "[Desktop Entry]
+Type=Application
+Categories=Office;
+Name=Camunda Modeler
+Comment=
+Icon=$TARGET_DIR/support/icon_48.png
+Exec=$TARGET_DIR/camunda-modeler %u
+Terminal=false" > $HOME/.local/share/applications/$MONIKER.desktop
+}
+
+install_mime_types() {
+  sudo xdg-mime install $TARGET_DIR/support/mime-types.xml
+}
+
+register_mime_handlers() {
+  for x in $MIME_TYPES; do
+    xdg-mime default $MONIKER.desktop $x
+  done
+}
+
+if [ -d "$TARGET_DIR" ]; then
+  echo Directory exists: $TARGET_DIR >&2
+  exit 1
+fi
+
 mkdir --parents $TARGET_DIR
-
 readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
 (
   cd $TEMP_DIR
@@ -24,6 +55,9 @@ readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
 
   echo -n Installing...
   mv --force camunda-modeler-$VERSION-linux-x64/* $TARGET_DIR
+  create_desktop_entry
+  install_mime_types
+  register_mime_handlers
   echo done
 )
 rm --recursive --force $TEMP_DIR
