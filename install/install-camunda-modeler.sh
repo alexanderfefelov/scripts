e9eb83387bcb66af16aadb9940ff919c649bd75a
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-04 14:28:30 +0300

    Use custom icons for Camunda Modeler files

--- install/install-camunda-modeler.sh
+++ install/install-camunda-modeler.sh
@@ -26,7 +26,43 @@ Terminal=false" > $HOME/.local/share/applications/$MONIKER.desktop
 }
 
 install_mime_types() {
-  xdg-mime install $TARGET_DIR/support/mime-types.xml
+  echo '<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="application/bpmn">
+    <comment>BPMN model</comment>
+    <acronym>BPMN</acronym>
+    <expanded-acronym>Business Process Model and Notation</expanded-acronym>
+    <glob pattern="*.bpmn"/>
+    <glob pattern="*.bpmn20.xml"/>
+    <sub-class-of type="application/xml"/>
+    <icon name="'$MONIKER'"/>
+    <root-XML namespaceURI="http://www.omg.org/spec/BPMN/20100524/MODEL" localName="definitions"/>
+  </mime-type>
+  <mime-type type="application/cmmn">
+    <comment>CMMN model</comment>
+    <acronym>CMMN</acronym>
+    <expanded-acronym>Case Management Model and Notation</expanded-acronym>
+    <glob pattern="*.cmmn"/>
+    <sub-class-of type="application/xml"/>
+    <icon name="'$MONIKER'"/>
+    <root-XML namespaceURI="http://www.omg.org/spec/CMMN/20151109/MODEL" localName="definitions"/>
+  </mime-type>
+  <mime-type type="application/dmn">
+    <comment>DMN model</comment>
+    <acronym>DMN</acronym>
+    <expanded-acronym>Decision Model and Notation</expanded-acronym>
+    <glob pattern="*.dmn"/>
+    <sub-class-of type="application/xml"/>
+    <icon name="'$MONIKER'"/>
+    <root-XML namespaceURI="http://www.omg.org/spec/DMN/20151101/dmn11.xsd" localName="definitions"/>
+    <root-XML namespaceURI="http://www.omg.org/spec/DMN/20151101/dmn.xsd" localName="definitions"/>
+  </mime-type>
+</mime-info>'> $MONIKER-mime.xml
+  xdg-mime install $MONIKER-mime.xml
+}
+
+install_mime_icon() {
+  xdg-icon-resource install --context mimetypes --size 48 $TARGET_DIR/support/icon_48.png $MONIKER
 }
 
 register_mime_handlers() {
@@ -57,6 +93,7 @@ readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
   mv --force camunda-modeler-$VERSION-linux-x64/* $TARGET_DIR
   create_desktop_entry
   install_mime_types
+  install_mime_icon
   register_mime_handlers
   echo done
 )
