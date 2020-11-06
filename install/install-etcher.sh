c3f8209cf8e56ca0f30de1cc9f3bb862bda51fb2
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-06 18:02:55 +0300

    Implement Etcher desktop integration

--- install/install-etcher.sh
+++ install/install-etcher.sh
@@ -7,8 +7,29 @@ set -e
 readonly MONIKER=etcher
 readonly VERSION=1.5.109
 readonly STUFF=balenaEtcher-$VERSION-x64.AppImage
-readonly TARGET_DIR=$HOME/bin
+readonly INSTALLER_DIR=$(dirname "$(realpath "$0")")
+readonly TARGET_DIR=$HOME/programs/$MONIKER
+
+create_desktop_entry() { # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+  echo "[Desktop Entry]
+Type=Application
+Categories=Utility;
+Name=Etcher
+Comment=
+Icon=$TARGET_DIR/etcher.png
+Exec=$TARGET_DIR/$MONIKER
+Terminal=false" > $HOME/.local/share/applications/$MONIKER.desktop
+}
+
+if [ -d "$TARGET_DIR" ]; then
+  echo Directory exists: $TARGET_DIR >&2
+  exit 1
+fi
+
+mkdir --parents $TARGET_DIR
 
 mkdir --parents $HOME/bin
 wget --quiet --output-document=$TARGET_DIR/$MONIKER https://github.com/balena-io/etcher/releases/download/v$VERSION/$STUFF
 chmod +x $TARGET_DIR/$MONIKER
+cp --force $INSTALLER_DIR/etcher.png $TARGET_DIR
+create_desktop_entry
