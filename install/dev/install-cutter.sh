d4b3a41a3c7935a0296ed5be49cc2c5e9f422b82
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-06 17:48:16 +0300

    Implement Cutter desktop integration

--- install/dev/install-cutter.sh
+++ install/dev/install-cutter.sh
@@ -7,8 +7,27 @@ set -e
 readonly MONIKER=cutter
 readonly VERSION=1.12.0
 readonly STUFF=Cutter-v$VERSION-x64.Linux.AppImage
-readonly TARGET_DIR=$HOME/bin
+readonly INSTALLER_DIR=$(dirname "$(realpath "$0")")
+readonly TARGET_DIR=$HOME/dev/$MONIKER
 
-mkdir --parents $HOME/bin
+create_desktop_entry() { # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+  echo "[Desktop Entry]
+Type=Application
+Categories=Development;
+Name=Cutter
+Comment=
+Icon=$TARGET_DIR/cutter.svg
+Exec=$TARGET_DIR/$MONIKER
+Terminal=false" > $HOME/.local/share/applications/$MONIKER.desktop
+}
+
+if [ -d "$TARGET_DIR" ]; then
+  echo Directory exists: $TARGET_DIR >&2
+  exit 1
+fi
+
+mkdir --parents $TARGET_DIR
 wget --quiet --output-document=$TARGET_DIR/$MONIKER https://github.com/radareorg/cutter/releases/download/v$VERSION/$STUFF
 chmod +x $TARGET_DIR/$MONIKER
+cp --force $INSTALLER_DIR/cutter.svg $TARGET_DIR
+create_desktop_entry
