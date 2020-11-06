b0242915d42e8ddcf6cd89b5d017ef48c6daf304
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-06 12:01:44 +0300

    Cosmetic changes

--- install/install-etcher.sh
+++ install/install-etcher.sh
@@ -4,9 +4,11 @@
 # a list, or a compound command returns a non-zero status
 set -e
 
+readonly MONIKER=etcher
 readonly VERSION=1.5.109
 readonly STUFF=balenaEtcher-$VERSION-x64.AppImage
+readonly TARGET_DIR=$HOME/bin
 
 mkdir --parents $HOME/bin
-wget --quiet --output-document=$HOME/bin/etcher https://github.com/balena-io/etcher/releases/download/v$VERSION/$STUFF
-chmod +x $HOME/bin/etcher
+wget --quiet --output-document=$TARGET_DIR/$MONIKER https://github.com/balena-io/etcher/releases/download/v$VERSION/$STUFF
+chmod +x $TARGET_DIR/$MONIKER
