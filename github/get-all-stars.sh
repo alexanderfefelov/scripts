fa542168dc692ef0efe46a0cd61b6fac21ef5993
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-28 06:23:50 +0300

    Cosmetic changes

--- github/get-all-stars.sh
+++ github/get-all-stars.sh
@@ -2,15 +2,30 @@
 
 readonly USERNAME=${1:-alexanderfefelov}
 
-readonly STARS=$(http --headers GET https://api.github.com/users/$USERNAME/starred per_page==1 | egrep '^link' | egrep -o 'page=[0-9]+' | tail -1 | cut -c6-)
+trap handle_int INT
+
+handle_int() {
+  echo Interrupted
+  exit 1
+}
+
+readonly STARS=$(
+  http --headers \
+    GET https://api.github.com/users/$USERNAME/starred \
+      per_page==1 \
+  | egrep '^link' \
+  | egrep -o 'page=[0-9]+' \
+  | tail -1 \
+  | cut -c6-
+)
 
 readonly PAGE_SIZE=100
-readonly PAGES=$(($STARS/$PAGE_SIZE+1))
+readonly PAGES=$(($STARS / $PAGE_SIZE + 1))
 for n in `seq $PAGES`; do
-  http --body GET \
-    https://api.github.com/users/$USERNAME/starred \
-    "Accept: application/vnd.github.v3.star+json" \
-    per_page==$PAGE_SIZE \
-    page==$n \
-    | jq -r '.[] | [.starred_at, .repo.full_name, .repo.created_at, .repo.updated_at, .repo.archived, .repo.language, .repo.stargazers_count, .repo.description] | @tsv'
+  http --body \
+    GET https://api.github.com/users/$USERNAME/starred \
+      "Accept: application/vnd.github.v3.star+json" \
+      per_page==$PAGE_SIZE \
+      page==$n \
+  | jq -r '.[] | [.starred_at, .repo.full_name, .repo.created_at, .repo.updated_at, .repo.archived, .repo.language, .repo.stargazers_count, .repo.description] | @tsv'
 done
