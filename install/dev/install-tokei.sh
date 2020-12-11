71a67a86e01aaa51da67094afd012a583541b73d
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-11 08:30:25 +0300

    Cleanup

--- install/dev/install-tokei.sh
+++ install/dev/install-tokei.sh
@@ -7,7 +7,6 @@ set -e
 readonly VERSION=12.0.4
 readonly STUFF=tokei-x86_64-unknown-linux-gnu.tar.gz
 
-mkdir --parents $HOME/bin
 wget --quiet \
   https://github.com/XAMPPRocky/tokei/releases/download/v$VERSION/$STUFF \
   --output-document=- \
