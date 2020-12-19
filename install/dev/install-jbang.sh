3d62c637272e70c4976f60fa057dcbc6bf768a7c
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-19 06:05:21 +0300

    Add JBang installer

---
+++ install/dev/install-jbang.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly MONIKER=jbang
+readonly VERSION=0.56.0
+readonly STUFF=jbang-$VERSION.zip
+readonly TARGET_DIR=$HOME/dev/$MONIKER
+
+if [ -d "$TARGET_DIR" ]; then
+  echo Directory exists: $TARGET_DIR >&2
+  exit 1
+fi
+
+mkdir --parents $TARGET_DIR
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+
+  echo -n Downloading...
+  wget --quiet https://github.com/jbangdev/jbang/releases/download/v$VERSION/$STUFF
+  echo done
+
+  echo -n Extracting...
+  unzip -qq $STUFF
+  echo done
+
+  echo -n Installing...
+  mv --force jbang-$VERSION/* $TARGET_DIR
+  echo done
+)
+rm --recursive --force $TEMP_DIR
+
+echo -n Configuring...
+echo "export JBANG_HOME=$TARGET_DIR
+export PATH=\$JBANG_HOME/bin:\$PATH" > $HOME/.profile.d/$MONIKER.sh
+echo done
