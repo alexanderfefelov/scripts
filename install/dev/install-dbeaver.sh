63c150eaca52f7721cac8883c68a7364db432751
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-23 10:18:10 +0300

    Update DBeaver

--- install/dev/install-dbeaver.sh
+++ install/dev/install-dbeaver.sh
@@ -5,7 +5,7 @@
 set -e
 
 readonly MONIKER=dbeaver
-readonly VERSION=7.3.0
+readonly VERSION=7.3.1
 readonly STUFF=dbeaver-ce-$VERSION-linux.gtk.x86_64.tar.gz
 readonly TARGET_DIR=$HOME/dev/$MONIKER
 
