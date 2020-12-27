c861c946fd98719ac09c0fc8fa062d16c417e1c9
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-27 20:39:19 +0300

    Add VirusTotal CLI installer

---
+++ install/ops/install-vt.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly VERSION=0.8.0
+readonly STUFF=Linux64.zip
+
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+
+  echo -n Downloading...
+  wget --quiet https://github.com/VirusTotal/vt-cli/releases/download/$VERSION/$STUFF
+  echo done
+
+  echo -n Extracting...
+  unzip -qq $STUFF
+  echo done
+
+  echo -n Installing...
+  mv --force vt $HOME/bin
+  echo done
+)
+rm --recursive --force $TEMP_DIR
