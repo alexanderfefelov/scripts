34bdc3f93cca0ca95fcbbbb2e670c4f5d8739d6f
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-01-06 22:21:18 +0300

    Implement calibre desktop integration

--- install/install-calibre.sh
+++ install/install-calibre.sh
@@ -5,7 +5,28 @@
 set -e
 
 readonly MONIKER=calibre
+readonly INSTALLER_DIR=$(dirname "$(realpath "$0")")
 readonly TARGET_DIR=$HOME/programs/$MONIKER
+
+create_desktop_entries() { # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+  echo "[Desktop Entry]
+Type=Application
+Categories=Office;
+Name=calibre
+Comment=
+Icon=$TARGET_DIR/calibre.png
+Exec=$TARGET_DIR/calibre %u
+Terminal=false" > $HOME/.local/share/applications/$MONIKER.desktop
+
+  echo "[Desktop Entry]
+Type=Application
+Categories=Office;
+Name=calibre E-book Viewer
+Comment=
+Icon=$TARGET_DIR/resources/images/viewer.png
+Exec=$TARGET_DIR/ebook-viewer %u
+Terminal=false" > $HOME/.local/share/applications/$MONIKER-viewer.desktop
+}
 
 if [ -d "$TARGET_DIR" ]; then
   echo Directory exists: $TARGET_DIR >&2
@@ -13,3 +34,5 @@ if [ -d "$TARGET_DIR" ]; then
 fi
 
 wget --quiet --output-document=- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin install_dir=$HOME/programs isolated=y
+cp --force $INSTALLER_DIR/calibre.png $TARGET_DIR
+create_desktop_entries
