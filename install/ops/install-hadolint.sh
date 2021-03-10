78b1eb1a5637972a2d886090a557dc0253e686a6
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-10 09:08:38 +0300

    Add hadolint installer

---
+++ install/ops/install-hadolint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly VERSION=1.23.0
+readonly STUFF=hadolint-Linux-x86_64
+
+wget --quiet \
+  https://github.com/hadolint/hadolint/releases/download/v$VERSION/$STUFF \
+  --output-document=$HOME/bin/hadolint
+chmod +x $HOME/bin/hadolint
