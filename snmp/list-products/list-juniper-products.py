8fb21c6b4eb40286986281da1265d8632de5ee80
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-11 23:10:51 +0300

    Fix Juniper product lister

--- snmp/list-products/list-juniper-products.py
+++ snmp/list-products/list-juniper-products.py
@@ -3,5 +3,7 @@
 from Juniper_mib_jnx_chas_defines_txt import MIB
 
 
-for node in MIB['nodes']:
+product_oid = MIB['nodes']['jnxProductName']['oid']
+products = [x for x in MIB['nodes'] if MIB['nodes'][x]['oid'].startswith(product_oid + '.')]
+for node in products:
     print(node, MIB['nodes'][node]['oid'])
