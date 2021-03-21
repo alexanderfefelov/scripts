3a1708384199c88fe903d0f8953744c179516e33
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-21 10:37:36 +0300

    Update Go

--- install/dev/install-go.sh
+++ install/dev/install-go.sh
@@ -5,7 +5,7 @@
 set -e
 
 readonly MONIKER=go
-readonly VERSION=1.15.8
+readonly VERSION=1.16.2
 readonly STUFF=go$VERSION.linux-amd64.tar.gz
 readonly TARGET_DIR=$HOME/dev/$MONIKER
 readonly WORKSPACE=$HOME/projects/$MONIKER
