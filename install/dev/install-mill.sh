1ebabf5a02dff4ab84c5beab469926f69264dbde
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-20 18:29:56 +0300

    Update Mill

--- install/dev/install-mill.sh
+++ install/dev/install-mill.sh
@@ -4,8 +4,8 @@
 # a list, or a compound command returns a non-zero status
 set -e
 
-readonly VERSION=0.9.5
-readonly STUFF=0.9.5
+readonly VERSION=0.9.6
+readonly STUFF=$VERSION
 
 wget --quiet \
   https://github.com/com-lihaoyi/mill/releases/download/$VERSION/$STUFF \
