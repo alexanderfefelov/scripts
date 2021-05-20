e72b0525c5c73bbc63bb735436afc40a84b92638
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-05-20 10:18:20 +0300

    Add minikube installer

---
+++ install/ops/kubernetes/install-minikube.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly VERSION=1.20.0
+readonly STUFF=minikube-linux-amd64
+
+wget --quiet --output-document=$HOME/bin/minikube https://github.com/kubernetes/minikube/releases/download/v$VERSION/$STUFF
+chmod +x $HOME/bin/minikube
