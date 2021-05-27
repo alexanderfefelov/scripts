c9c379a46405679c7061a07276bf151ae82ac37e
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-05-27 04:28:53 +0300

    Add Azure Data Studio installer

---
+++ install/dev/install-azure-data-studio.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly MONIKER=azure-data-studio
+readonly VERSION=1.29.0
+readonly STUFF=azuredatastudio-linux-$VERSION.tar.gz
+readonly TARGET_DIR=$HOME/dev/$MONIKER
+
+create_desktop_entry() { # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+  echo "[Desktop Entry]
+Type=Application
+Categories=Development;
+Name=Azure Data Studio
+Comment=
+Icon=$TARGET_DIR/resources/app/resources/linux/code.png
+Exec=$TARGET_DIR/azuredatastudio
+Terminal=false" > $HOME/.local/share/applications/azure-data-studio.desktop
+}
+
+if [ -d "$TARGET_DIR" ]; then
+  echo Directory exists: $TARGET_DIR >&2
+  exit 1
+fi
+
+mkdir --parents $TARGET_DIR
+readonly TEMP_DIR=$(mktemp --directory -t delete-me-XXXXXXXXXX)
+(
+  cd $TEMP_DIR
+
+  echo -n Downloading...
+  wget --quiet https://sqlopsbuilds.azureedge.net/stable/0f5cfdc2c8c2433028842e16dc5ac1a65da24292/$STUFF
+  echo done
+
+  echo -n Extracting...
+  tar --extract --gzip --file=$STUFF
+  echo done
+
+  echo -n Installing...
+  mv --force azuredatastudio-linux-x64/* $TARGET_DIR
+  create_desktop_entry
+  echo done
+)
+rm --recursive --force $TEMP_DIR
