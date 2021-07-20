c59444e37a0b6e4dc36cd86df1d2a68c4ae0c340
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-07-20 11:06:48 +0300

    Update Camunda Modeler

--- install/install-camunda-modeler.sh
+++ install/install-camunda-modeler.sh
@@ -5,7 +5,7 @@
 set -e
 
 readonly MONIKER=camunda-modeler
-readonly VERSION=4.8.1
+readonly VERSION=4.9.0
 readonly STUFF=camunda-modeler-$VERSION-linux-x64.tar.gz
 readonly TARGET_DIR=$HOME/programs/$MONIKER
 readonly MIME_TYPES="
